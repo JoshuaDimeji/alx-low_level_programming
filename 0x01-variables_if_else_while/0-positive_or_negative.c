ae3c514ab3b639d0b5e45874b5a2d431afa66f91
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-03 10:41:51 -0700

    pushing through alx

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 
 /**
  * main - function to determine if a number is positive or negative or zero
- * Return: Always 0 (Succcessful)
+ * Return: Always 0 (Successful)
  */
 
 int main(void)
 {
-
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n",  n;)
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
 	}
 	else
