ff8fc2f267784884cff309619c9808a8abbe440c
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-03 04:37:26 -0700

    Josh can code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - code to put a message
- *   * Return: Always 0
- *    */
+    * main - code to put a message
+    * Return: Always 0
+    * 
+    */
 
 int main(void)
 {
