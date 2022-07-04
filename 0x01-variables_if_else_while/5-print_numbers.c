8d891079e7e656c92c5e70f0e06f0029744a43aa
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-04 12:22:15 -0700

    Jay must code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10
+ * Reurtn: 0 (Successful)
+ */
+int main(void)
+{
+
+	char d = '0';
+
+	while (d <= '9')
+	{
+	putchar(d);
+	d++;
+	}
+	putchar('\n');
+	return (0);
+}
