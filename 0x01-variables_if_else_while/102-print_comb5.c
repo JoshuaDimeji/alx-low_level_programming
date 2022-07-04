47d71d3a3bff601b082eb274a8b02d01a28d9056
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-04 14:18:57 -0700

    processing alx

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function to print all possible combinations of two two-digit numbers
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+	for (j = 0; j <= 99; j++)
+	{
+	if (i < j)
+	{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+
+	if (i != 98 || j != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
