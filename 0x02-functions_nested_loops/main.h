967396e5b2e3c06636caf8941f045f1d18158aac
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-05 18:53:55 -0700

    function that prints the alphabet in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * header file containing all functions used in this current directory
+ * file: main.h
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet: function to print alphabet
+ */
