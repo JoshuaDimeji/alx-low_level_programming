ea46906648b92005ac226d9aa67a38101546b4ad
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-07-19 21:28:04 -0700

    ALX

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,13 @@
 void *_memset(void *s, int c, size_t n)
 {
 
-	unsigned int index;
-	unsigned char *memory = s, value = c;
-
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	unsigned int count = 0;
+	
+	while (count < n)
+	{
+		s[count] = c;
+		count++;
+	}
+	
+	return (s);
 }
