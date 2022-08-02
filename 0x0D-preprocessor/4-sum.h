dec367b6e45ee6ebcafe0c66cc9fe3ca58e09b9c
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-08-02 16:23:15 -0700

    function-like macro that computes addition

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * Header file that defines a function-like macro SUM(x, y)
+ * @x: the first value
+ * @y: the second value
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
