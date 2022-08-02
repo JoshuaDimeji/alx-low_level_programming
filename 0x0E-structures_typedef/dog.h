29deba6900ff99ad8be598316552c6221ff76b10
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-08-02 05:24:46 -0700

    a new type for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a type describing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
