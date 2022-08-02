b6f7102a6a344725f3af70a028800478636eac87
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-08-02 16:09:42 -0700

    ALX

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
+/**
+ * constructing a function-like macro that finds absolute values
+ * @x: the input
+ */
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
