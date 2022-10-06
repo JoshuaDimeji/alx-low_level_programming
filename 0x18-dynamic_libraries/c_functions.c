935aa49431f9b7b9d8a8bf6441dc8abb1e3fdd03
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-10-05 18:19:47 -0700

    dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/c_functions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * add - function to add two integers
+ * sub - function to subtract two integers
+ * mul - function to multiply two integers
+ * div - function to divide two integers
+ * mod - function to find the remainder when you divide two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: the value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
